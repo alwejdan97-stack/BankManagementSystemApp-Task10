3220519f61d72254d541745cb4a9b637ef3c8e1e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:05:05 +0400

    add addCustomer(@RequestBody Customer newCustomer) method to accept new customer

--- demo/src/main/java/BankManagementSystem/demo/Controllers/CustomerController.java
+++ demo/src/main/java/BankManagementSystem/demo/Controllers/CustomerController.java
@@ -1,9 +1,21 @@
 package BankManagementSystem.demo.Controllers;
 
+import BankManagementSystem.demo.Entities.Customer;
+import BankManagementSystem.demo.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("addCustomer")
+    public Customer addCustomer(@RequestBody Customer newCustomer){
+        return customerService;
+    }
 }
