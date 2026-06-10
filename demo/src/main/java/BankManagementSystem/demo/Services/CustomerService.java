93ab3073a24f5589922072300750e2c191fa1378
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:34:48 +0400

    add findCustomerbyName(String name)

--- demo/src/main/java/BankManagementSystem/demo/Services/CustomerService.java
+++ demo/src/main/java/BankManagementSystem/demo/Services/CustomerService.java
@@ -30,5 +30,7 @@ public class CustomerService {
         return customerRepository.findCustomerById(id);
     }
 
-
+    public Customer findCustomerbyName(String name){
+        return customerRepository.findCustomerbyName(name);
+    }
 }
