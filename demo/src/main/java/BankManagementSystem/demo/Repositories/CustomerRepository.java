cf68420f3368bf96434cc306b8a47ea8527d71ea
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:21:24 +0400

    getAllCustomers() method

--- demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
@@ -2,11 +2,13 @@ package BankManagementSystem.demo.Repositories;
 
 import BankManagementSystem.demo.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    @Query("SELECT C FROM Customer C WHERE C.customerName=NOTNULL")
     List<Customer> getAllCustomers();
 }
