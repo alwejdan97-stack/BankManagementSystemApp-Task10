41e7061d8cc34a7aa36fca48b362eb1502e6eb40
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:17:20 +0400

    getAllCustomers() method

--- demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
@@ -4,7 +4,9 @@ import BankManagementSystem.demo.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-
+    List<Customer> getAllCustomers();
 }
