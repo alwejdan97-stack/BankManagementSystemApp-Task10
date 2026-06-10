a8f129b4b50ec34212ab71a827f2ac7caaf5eeae
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:00:00 +0400

    add required annotation

--- demo/src/main/java/BankManagementSystem/demo/Services/CustomerService.java
+++ demo/src/main/java/BankManagementSystem/demo/Services/CustomerService.java
@@ -1,4 +1,11 @@
 package BankManagementSystem.demo.Services;
 
+import BankManagementSystem.demo.Repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
 }
