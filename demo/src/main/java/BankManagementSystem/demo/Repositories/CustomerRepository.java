c6db056da61ae32deeb63df453899bc8dc6aa46b
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 10:56:27 +0400

    create CustomerRepository interface

--- demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
@@ -1,11 +1,4 @@
-/*
 package BankManagementSystem.demo.Repositories;
 
-import BankManagementSystem.demo.Entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CustomerRepository extends JpaRepository<Customer, Integer>{
+public interface CustomerRepository {
 }
-*/
