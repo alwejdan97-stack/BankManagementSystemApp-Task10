93ec7ac76498a7f1f10fc83836ee9a604419f889
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 10:57:27 +0400

    make the interface extens JpaRepository

--- demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
@@ -1,4 +1,9 @@
 package BankManagementSystem.demo.Repositories;
 
-public interface CustomerRepository {
+import BankManagementSystem.demo.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
