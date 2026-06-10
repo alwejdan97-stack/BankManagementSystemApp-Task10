9d224d3a4d768a709e3f3392dd5a0b0d557719df
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 11:30:42 +0400

    add add findCustomerById(Integer id)

--- demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/BankManagementSystem/demo/Repositories/CustomerRepository.java
@@ -3,7 +3,9 @@ package BankManagementSystem.demo.Repositories;
 import BankManagementSystem.demo.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
     //@Query("SELECT C FROM Customer C WHERE C.customerName=NOTNULL")
     List<Customer> getAllCustomers();
+
+    @Query("SELECT C FROM Customer C WHERE C.customerId=:customerId")
+    Customer findCustomerById(@Param ("customerId") Integer id);
 }
