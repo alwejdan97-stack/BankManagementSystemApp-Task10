daec4a9accac80b7074fe5744c653cb7ce285978
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-10 10:39:14 +0400

    store customer information as private feilds

--- demo/src/main/java/BankManagementSystem/demo/Entities/Customer.java
+++ demo/src/main/java/BankManagementSystem/demo/Entities/Customer.java
@@ -1,4 +1,10 @@
 package BankManagementSystem.demo.Entities;
 
 public class Customer {
+    private int customerId;
+    private String customerName;
+    private int accountNumber;
+    private double balance;
+    private String email;
+    private String phoneNumber;
 }
